85d476755001c694f512c636cb0e517c82242830
TanvirGalive <galivetanvir@gmail.com>
2018-05-15 12:11:50 +0600

    This is initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".ajax-mailchimp").ajaxChimp({
+        	url: 'https://tanvirgalive.us18.list-manage.com/subscribe/post?u=2007b54263e20fc4552a30852&amp;id=0e0f172ac7'
+        });
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
